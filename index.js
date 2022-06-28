682896289689405dd3a4bd9e9386120d497cdfc2
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-06-28 12:46:24 +0200

    parsing de fou

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { Sequelize } = require("sequelize");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
 // Declare sql infos
 const sequelize = new Sequelize(
@@ -65,10 +65,10 @@ const addDevice = async (device) => {
   await Device.create({
     date: device.date,
     name: device.name,
-    temp: device.temp,
+    temp: device.temperature,
     humidity: device.humidity,
-    airPressure: device.airPressure,
-    lightLevel: device.lightLevel,
+    airPressure: device.pressure,
+    lightLevel: device.light,
     batteryLevel: device.batteryLevel,
     fireWarning: device.fireWarning,
   }).catch((err) => {
@@ -94,8 +94,53 @@ app.get("/getdevices", (req, res) => {
   try {
     sequelize.authenticate();
     sequelize.query("SELECT * FROM `devices`").then(([results, metadata]) => {
-      console.log(results);
-      res.send(results);
+      let data = results;
+
+      let finalData = {
+        allData: [
+          {
+            deviceName: data[0].name,
+            deviceData: {
+              temp: [],
+              humidity: [],
+              airPressure: [],
+              lightLevel: [],
+              batteryLevel: [],
+              fireWarning: [],
+            },
+          },
+        ],
+      };
+
+      for (let i = 0; i < data.length; i++) {
+        finalData.allData[0].deviceData.temp.push({
+          date: data[i].date,
+          value: data[i].temp,
+        });
+        finalData.allData[0].deviceData.humidity.push({
+          date: data[i].date,
+          value: data[i].humidity,
+        });
+        finalData.allData[0].deviceData.airPressure.push({
+          date: data[i].date,
+          value: data[i].airPressure,
+        });
+        finalData.allData[0].deviceData.lightLevel.push({
+          date: data[i].date,
+          value: data[i].lightLevel,
+        });
+        finalData.allData[0].deviceData.batteryLevel.push({
+          date: data[i].date,
+          value: data[i].batteryLevel,
+        });
+        finalData.allData[0].deviceData.fireWarning.push({
+          date: data[i].date,
+          value: data[i].fireWarning,
+        });
+      }
+
+      console.log(finalData);
+      res.send(finalData);
     });
   } catch (error) {
     console.error("Error at line selection : ", error);
@@ -107,10 +152,24 @@ app.get("/getdevices", (req, res) => {
 app.post("/adddevice", function (req, res) {
   console.log(req.body); // your JSON
 
-  const device = req.body;
+  let device = req.body;
+  device.name = "deviceHome";
 
-  console.log("device name : " + device);
-  // addDevice(device);
+  const dt = new Date();
+  const finalDate = `${(dt.getMonth() + 1).toString().padStart(2, "0")}/${dt
+    .getDate()
+    .toString()
+    .padStart(2, "0")}/${dt.getFullYear().toString().padStart(4, "0")} ${dt
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${dt.getMinutes().toString().padStart(2, "0")}`;
+
+  device.date = finalDate;
+  device.batteryLevel = 3.7;
+
+  console.log(device.name);
+  console.log(device.date);
+  addDevice(device);
 
   res.send(`${device} inserted in ${process.env.DB_NAME}`);
 });
@@ -128,14 +187,85 @@ app.listen(port, () => {
   console.log(`Cuicuiot api listening on port ${port}`);
 });
 
-let truc = {
-  id: 1,
-  date: "28/06/2022",
-  name: "device1",
-  temp: 24,
-  humidity: 56,
-  airPressure: 1024,
-  lightLevel: 230,
-  batteryLevel: 22,
-  fireWarning: 0,
+/*
+let data = [
+  {
+    id: 1,
+    date: "28/06/2022",
+    name: "device1",
+    temp: 24,
+    humidity: 56,
+    airPressure: 1024,
+    lightLevel: 230,
+    batteryLevel: 22,
+    fireWarning: 0,
+  },
+  {
+    id: 4,
+    date: "28/06/2022",
+    name: "deviceHome",
+    temp: 26.554,
+    humidity: 50.482,
+    airPressure: 990,
+    lightLevel: 173,
+    batteryLevel: 3.7,
+    fireWarning: 0,
+  },
+  {
+    id: 5,
+    date: "06/28/2022 12:15",
+    name: "deviceHome",
+    temp: 26.554,
+    humidity: 50.482,
+    airPressure: 990,
+    lightLevel: 173,
+    batteryLevel: 3.7,
+    fireWarning: 0,
+  },
+];
+
+let finalData = {
+  allData: [
+    {
+      deviceName: data[0].name,
+      deviceData: {
+        temp: [],
+        humidity: [],
+        airPressure: [],
+        lightLevel: [],
+        batteryLevel: [],
+        fireWarning: [],
+      },
+    },
+  ],
 };
+
+for (let i = 0; i < data.length; i++) {
+  finalData.allData[0].deviceData.temp.push({
+    date: data[i].date,
+    value: data[i].temp,
+  });
+  finalData.allData[0].deviceData.humidity.push({
+    date: data[i].date,
+    value: data[i].humidity,
+  });
+  finalData.allData[0].deviceData.airPressure.push({
+    date: data[i].date,
+    value: data[i].airPressure,
+  });
+  finalData.allData[0].deviceData.lightLevel.push({
+    date: data[i].date,
+    value: data[i].lightLevel,
+  });
+  finalData.allData[0].deviceData.batteryLevel.push({
+    date: data[i].date,
+    value: data[i].batteryLevel,
+  });
+  finalData.allData[0].deviceData.fireWarning.push({
+    date: data[i].date,
+    value: data[i].fireWarning,
+  });
+}
+
+console.log(finalData.allData[0].deviceData);
+*/
